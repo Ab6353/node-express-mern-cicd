e95e654b3ffa5818063d88a64717a162346d19b0
Ashutosh Behera <91386098+Ab6353@users.noreply.github.com>
2022-03-27 22:26:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('process.env.MONGODB_DEV_URI');
+  mongoose.connect('mongodb://10.0.143.151:27017,10.0.131.149:27017,10.0.132.0:27017/conduit?replicaSet=rs0');
   mongoose.set('debug', true);
 }
 
