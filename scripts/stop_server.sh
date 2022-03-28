8ddcf112a107ef8f66cbb5e2091fc26be19ec153
Ashutosh Behera <91386098+Ab6353@users.noreply.github.com>
2022-03-28 11:04:22 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-pid=$(sudo pm2 pid app)
-if [ $pid == 0 ]; then
-   echo "Application is stopped"
-else
-   echo "Application is running"
-   sudo pm2 stop app/app.js
-fi
+sudo pm2 stop app.js
