11f4039bcf3ac7a6625ffa7a012133f557bc2da3
Ashutosh Behera <91386098+Ashutosh-aws@users.noreply.github.com>
2022-08-08 18:08:55 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/node-express-mern-cicd
-sudo pm2 start app.js -o="/home/ubuntu/.pm2/logs/app-out.log" -e="/home/ubuntu/.pm2/logs/app-error.log"
+sudo pm2 start app.js
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-ashutosh -s
 sudo pm2 startup
 sudo pm2 save
